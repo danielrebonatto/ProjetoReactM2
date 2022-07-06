6f97b930821ff14e1b25420d1284938fcfb92f05
Casio <casiokobs12@gmail.com>
2022-07-06 11:29:41 -0300

    ajustado altura da img estrela

--- client/src/components/home/HomePage.js
+++ client/src/components/home/HomePage.js
@@ -5,7 +5,7 @@ const HomePage = () => {
     <div>
       <h2> Home </h2>
       <img width='100px' src='https://phoneky.co.uk/thumbs/screensavers/down/games/supermario_hlq6z1q4.gif'></img>
-      <img class='estrela-mario' width='50px' src='https://pa1.narvii.com/6974/f1ed2619479f393666e8b1973e2556415f5767f5r1-320-320_hq.gif'></img>
+      <img className='estrela-mario' width='50px' src='https://pa1.narvii.com/6974/f1ed2619479f393666e8b1973e2556415f5767f5r1-320-320_hq.gif'></img>
       
       <p>
         Bem vindo a Game Teca, aonde voce pode salvar os seus jogos favoritos.
