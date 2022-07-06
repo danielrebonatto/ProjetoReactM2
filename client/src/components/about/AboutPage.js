c59a3a8a55c0f26e1407da819cb27451ca488cb1
Casio <casiokobs12@gmail.com>
2022-07-06 09:35:12 -0300

    alterado a msg na tela de about

--- client/src/components/about/AboutPage.js
+++ client/src/components/about/AboutPage.js
@@ -5,11 +5,7 @@ const AboutPage = () => {
     <div>
       <h2> Sobre nós </h2>
       <p>
-<<<<<<< HEAD
-        Esse site tem como objetivo cadastros de Games e visualização, com controle de autenticação e permissões.
-=======
-        A Game Teca foi criada com o intuito de compartilhar jogos de qualidade com a comunidade, facilitando a busca de novas experiências.
->>>>>>> f2f7192e34e49f9f63c514cfa929f884b0b7e361
+      A Game Teca foi criada com o intuito de compartilhar jogos de qualidade com a comunidade, facilitando a busca de novas experiências.
       </p>
     </div>
   )
