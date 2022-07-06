02d53fe704221501b82c34b206d7c68291702d5e
Casio <casiokobs12@gmail.com>
2022-07-06 09:24:29 -0300

    mudança na homepage

--- client/src/components/home/HomePage.js
+++ client/src/components/home/HomePage.js
@@ -3,9 +3,10 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div>
-      <h2> Homepage </h2>
+      <h2> Home </h2>
+      <img width='100px' src='https://phoneky.co.uk/thumbs/screensavers/down/games/supermario_hlq6z1q4.gif'></img>
       <p>
-        This is the homepage.
+        Bem vindo a Game Teca, aonde voce pode salvar os seus jogos favoritos.
       </p>
     </div>
   )
